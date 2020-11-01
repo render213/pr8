33e4d04d2efae10e0579e6c33e689d3867365d05
render01 <54182692+render01@users.noreply.github.com>
2020-11-01 15:32:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace pr8
+{
+    class Program
+    {
+        static void Counting()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("Count: {0} - Thread' {1} ", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart starter = new ThreadStart(Counting);
+            Thread first = new Thread(starter);
+            Thread second = new Thread(starter);
+            first.Start(); second.Start();
+            first.Join(); second.Join();
+            Console.Read();
+        }
+    }
+}
